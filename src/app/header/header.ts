68ce0145911bbf90ead629f23f3995be84dafc4b
ash <ashzhenbi@gmail.com>
2025-08-06 14:42:01 +1000

    header link update

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ViewportScroller } from '@angular/common'; // 导入 ViewportScroller
 
 @Component({
   selector: 'app-header',
@@ -8,4 +9,17 @@ import { RouterModule } from '@angular/router';
   templateUrl: './header.html',
   styleUrls: ['./header.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private viewportScroller: ViewportScroller) {} // 注入 ViewportScroller
+
+  /**
+   * 平滑滚动到指定ID的元素
+   * @param anchor 要滚动到的元素ID
+   */
+  scrollToAnchor(anchor: string): void {
+    // 使用 setTimeout 确保 DOM 渲染完成后再滚动，尤其是在路由变化后
+    setTimeout(() => {
+      this.viewportScroller.scrollToAnchor(anchor);
+    }, 0);
+  }
+}
